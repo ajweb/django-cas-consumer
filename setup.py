baae8683401d2fe0401b902e6064ea9a27998fe4
ajj <a@ajweb.eu>
2014-05-23 17:40:54 +0200

    corrected setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   version = '0.1',
   description = 'Fork of CAS Consumer for django',
   author = 'Versio4',
-  author_email = 'peterldowns@gmail.com',
+  author_email = 'aj@versio4.com',
   url = 'http://versio4.com', 
   # this needs to point to release branch
   download_url = 'https://bitbucket.org/versio4/av22-cas-consumer/get/release.zip',
